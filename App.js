6c64cfe5138c82f07da109a82cebf5be5c79ae64
Omar Mohamed <ofattouh2@gmail.com>
2021-05-03 03:11:39 -0400

    Add tabs/navigation support

--- App.js
+++ App.js
@@ -6,135 +6,68 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
-import {
-  FlatList,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  ActivityIndicator,
-} from 'react-native';
+import React from 'react';
+import {Platform} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Home from './Screens/Home';
+import Detail from './Screens/Detail';
+import Settings from './Screens/Settings';
 
-const data = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+const HomeStack = createStackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {title: 'Home'},
   },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
-
-const Item = ({item, onPress, backgroundColor, textColor}) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-    <Text style={[styles.title, textColor]}>{item.title}</Text>
-  </TouchableOpacity>
-);
-
-const App = () => {
-  const [selectedId, setSelectedId] = useState(null);
-
-  const renderHeader = () => {
-    return (
-      <View style={styles.header}>
-        <Text>Header</Text>
-      </View>
-    );
-  };
-
-  const renderFooter = () => {
-    return (
-      <View style={styles.footer}>
-        <Text>Footer</Text>
-        <ActivityIndicator animating size="large" />
-      </View>
-    );
-  };
-
-  const renderSeparator = () => {
-    return <View style={styles.separator} />;
-  };
-
-  const renderItem = ({item}) => {
-    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
-    const color = item.id === selectedId ? 'white' : 'black';
-
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={{backgroundColor}}
-        textColor={{color}}
-      />
-    );
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        ListHeaderComponent={renderHeader}
-        ListFooterComponent={renderFooter}
-        ItemSeparatorComponent={renderSeparator}
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-        extraData={selectedId}
-      />
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: '#ecf0f1',
-  },
-  item: {
-    padding: 10,
-    marginVertical: 15,
-    marginHorizontal: 10,
-    height: 150,
-  },
-  title: {
-    fontSize: 15,
-  },
-  separator: {
-    height: 1,
-    width: '95%',
-    backgroundColor: '#CED0CE',
-    marginLeft: '2%',
-  },
-  footer: {
-    borderTopWidth: 1,
-    borderColor: '#CED0CE',
-    backgroundColor: '#CECE',
-    padding: 10,
-    height: 70,
-  },
-  header: {
-    backgroundColor: '#CECE',
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    height: 150,
+  Detail: {
+    screen: Detail,
+    navigationOptions: {title: 'Detail'},
   },
 });
 
-export default App;
+const SettingsStack = createStackNavigator({
+  Settings: {
+    screen: Settings,
+    navigationOptions: {title: 'Settings'},
+  },
+});
+
+const AppNavigator = createBottomTabNavigator(
+  {
+    Home: HomeStack,
+    Settings: SettingsStack,
+  },
+  {
+    // initial route rendered when app first mounts
+    initialRouteName: 'Home',
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({tintColor}) => {
+        const {routeName} = navigation.state;
+        let iconName;
+
+        if (routeName === 'Home') {
+          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-home`;
+        } else if (routeName === 'Settings') {
+          iconName = `${Platform.OS === 'ios' ? 'ios' : 'md'}-settings`;
+        }
+
+        return <Ionicons name={iconName} size={20} color={tintColor} />;
+      },
+      tabBarOptions: {
+        activeTintColor: 'blue',
+        inactiveTintColor: '#556',
+      },
+    }),
+  },
+);
+
+export default createAppContainer(AppNavigator);
 
 // npx react-native init MuslimPrayerCompanion
+// npm install react-navigation react-navigation-stack react-navigation-tabs
+// npm install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
 // cd ios // pod install
 // https://reactnative.dev/docs/flatlist
 // https://reactnative.dev/docs/sectionlist (sectioned)
---
+++ Components/FlatList/FlastList.js
@@ -0,0 +1,138 @@
+import React, {useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ActivityIndicator,
+} from 'react-native';
+
+const data = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({item, onPress, backgroundColor, textColor}) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    <Text style={[styles.title, textColor]}>{item.title}</Text>
+  </TouchableOpacity>
+);
+
+const App = () => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const renderHeader = () => {
+    return (
+      <View style={styles.header}>
+        <Text>Header</Text>
+      </View>
+    );
+  };
+
+  const renderFooter = () => {
+    return (
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+        <ActivityIndicator animating size="large" />
+      </View>
+    );
+  };
+
+  const renderSeparator = () => {
+    return <View style={styles.separator} />;
+  };
+
+  const renderItem = ({item}) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={{backgroundColor}}
+        textColor={{color}}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        ListHeaderComponent={renderHeader}
+        ListFooterComponent={renderFooter}
+        ItemSeparatorComponent={renderSeparator}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#ecf0f1',
+  },
+  item: {
+    padding: 10,
+    marginVertical: 15,
+    marginHorizontal: 10,
+    height: 150,
+  },
+  title: {
+    fontSize: 15,
+  },
+  separator: {
+    height: 1,
+    width: '95%',
+    backgroundColor: '#CED0CE',
+    marginLeft: '2%',
+  },
+  footer: {
+    borderTopWidth: 1,
+    borderColor: '#CED0CE',
+    backgroundColor: '#CECE',
+    padding: 10,
+    height: 70,
+  },
+  header: {
+    backgroundColor: '#CECE',
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    height: 150,
+  },
+});
+
+export default App;
+
+// npx react-native init MuslimPrayerCompanion
+// npm install react-navigation react-navigation-stack react-navigation-tabs
+// npm install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+// cd ios // pod install
+// https://reactnative.dev/docs/flatlist
+// https://reactnative.dev/docs/sectionlist (sectioned)
+// https://reactnative.dev/docs/virtualizedlist (mutable data instead of plain arrays)
+// https://reactnativeelements.com/docs/listitem/ (contacts with avatar)
+// https://reactnativeelements.com/docs/searchbar/
+// https://medium.com/react-native-development/how-to-use-the-flatlist-component-react-native-basics-92c482816fe6
