f2f84d612d129017527e9c8174c0c4f4136a3161
Omar Mohamed <ofattouh2@gmail.com>
2021-05-04 21:01:20 -0400

    changes

--- App.js
+++ App.js
@@ -20,13 +20,31 @@ function HomeScreen() {
   );
 }
 
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview'}}
+        />
+
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{title: 'My Details'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
