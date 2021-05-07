d6fb846c0ce771de150c31ebf41f5a0821756e30
Omar Mohamed <ofattouh2@gmail.com>
2021-05-06 20:21:22 -0400

    changes

--- App.js
+++ App.js
@@ -18,28 +18,40 @@ function HomeScreen({navigation}) {
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
       />
     </View>
   );
 }
 
-function DetailsScreen({navigation}) {
+function DetailsScreen({route, navigation}) {
+  /* 2. Get the param */
+  const {itemId, otherParam, itemId2} = route.params;
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Text>initialParams: {JSON.stringify(itemId2)}</Text>
 
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
       />
 
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
     </View>
   );
 }
@@ -60,6 +72,7 @@ function App() {
           name="Details"
           component={DetailsScreen}
           options={{title: 'My Details'}}
+          initialParams={{itemId2: 42}}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -70,6 +83,7 @@ export default App;
 
 // npx react-native init MyApp
 // cd ios // pod install OR npx pod-install ios
+// https://reactnavigation.org/docs/navigation-prop/
 // https://reactnative.dev/docs/flatlist
 // https://reactnative.dev/docs/sectionlist (sectioned)
 // https://reactnative.dev/docs/virtualizedlist (mutable data instead of plain arrays)
