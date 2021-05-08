a645684dcb87fa4c86e4310f884478073955bb23
Omar Mohamed <ofattouh2@gmail.com>
2021-05-08 19:01:11 -0400

    changes

--- App.js
+++ App.js
@@ -8,46 +8,79 @@
 
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+
+function HomeHeaderSection() {
+  return (
+    <View style={{flex: 1}}>
+      <Image
+        style={{width: 30, height: 30}}
+        source={{uri: 'https://picsum.photos/30/30.jpg'}}
+      />
+    </View>
+  );
+}
+
+function ProfileHeaderSection() {
+  return (
+    <View style={{flex: 1}}>
+      <Text>Profile</Text>
+
+      <Image
+        style={{width: 30, height: 30}}
+        source={{uri: 'https://picsum.photos/30/30.jpg'}}
+      />
+    </View>
+  );
+}
 
 function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+
       <Button
-        title="Go to Details"
+        title="Go to My Details"
         onPress={() => {
-          /* 1. Navigate to the Details route with params */
-          navigation.navigate('Details', {
+          navigation.navigate('My Details', {
             itemId: 86,
             otherParam: 'anything you want here',
           });
         }}
+      />
+
+      <Image
+        style={{width: 300, height: 300}}
+        source={{uri: 'https://picsum.photos/300/300.jpg'}}
       />
     </View>
   );
 }
 
 function DetailsScreen({route, navigation}) {
-  /* 2. Get the param */
   const {itemId, otherParam, itemId2} = route.params;
 
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Details Screen</Text>
+      <Text>My Details Screen</Text>
       <Text>itemId: {JSON.stringify(itemId)}</Text>
       <Text>otherParam: {JSON.stringify(otherParam)}</Text>
       <Text>initialParams: {JSON.stringify(itemId2)}</Text>
 
       <Button
-        title="Go to Details... again"
+        title="Go to My Details... again"
         onPress={() =>
-          navigation.push('Details', {
+          navigation.push('My Details', {
             itemId: Math.floor(Math.random() * 100),
           })
         }
+      />
+
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
       />
 
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
@@ -56,23 +89,65 @@ function DetailsScreen({route, navigation}) {
   );
 }
 
+function ProfileScreen({navigation}) {
+  const [count, setCount] = React.useState(0);
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => setCount(c => c + 1)} title="Update count" />
+      ),
+    });
+  }, [navigation]);
+
+  return <Text>Count: {count}</Text>;
+}
+
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontFamily: 'Arial',
+          },
+        }}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: 'Overview'}}
+          options={{
+            headerTitle: props => <HomeHeaderSection {...props} />,
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Button"
+                color="#fff"
+              />
+            ),
+          }}
         />
 
         <Stack.Screen
-          name="Details"
+          name="My Details"
           component={DetailsScreen}
-          options={{title: 'My Details'}}
+          options={({route}) => ({title: route.params.name})}
           initialParams={{itemId2: 42}}
+        />
+
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={({}) => ({
+            headerTitle: props => <ProfileHeaderSection {...props} />,
+          })}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -84,6 +159,8 @@ export default App;
 // npx react-native init MyApp
 // cd ios // pod install OR npx pod-install ios
 // https://reactnavigation.org/docs/navigation-prop/
+// https://github.com/vonovak/react-navigation-header-buttons
+// https://github.com/oblador/react-native-vector-icons
 // https://reactnative.dev/docs/flatlist
 // https://reactnative.dev/docs/sectionlist (sectioned)
 // https://reactnative.dev/docs/virtualizedlist (mutable data instead of plain arrays)
