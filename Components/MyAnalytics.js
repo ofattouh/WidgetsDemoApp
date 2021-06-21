a7cf56a65c7ea36f4a0e0dc1e33e9572f40e53f1
Omar M <ofattouh2@gmail.com>
2021-06-20 20:15:27 -0400

    route navigations code

--- Components/MyAnalytics.js
+++ Components/MyAnalytics.js
@@ -35,38 +35,56 @@ export default function MyAnalytics() {
   const navigationRef = React.useRef();
 
   return (
-    <NavigationContainer
-      independent={true} // warning, only 1 navigation container is allowed at root of app
-      ref={navigationRef}
-      onReady={() =>
-        (routeNameRef.current = navigationRef.current.getCurrentRoute().name)
-      }
-      onStateChange={async () => {
-        const previousRouteName = routeNameRef.current;
-        const currentRouteName = navigationRef.current.getCurrentRoute().name;
+    <View style={styles.container2}>
+      <NavigationContainer
+        independent={true} // warning, only 1 navigation container is allowed at root of app
+        ref={navigationRef}
+        /* documentTitle={{
+          // customize page title text only on web
+          formatter: (options, route) =>
+            `${options?.title ?? route?.name} - My Muslim Pro App`,
+        }} */
+        onReady={() =>
+          (routeNameRef.current = navigationRef.current.getCurrentRoute().name)
+        }
+        onStateChange={async () => {
+          const previousRouteName = routeNameRef.current;
+          const currentRouteName = navigationRef.current.getCurrentRoute().name;
 
-        if (previousRouteName !== currentRouteName) {
-          // The line below uses the expo-firebase-analytics tracker
-          // https://docs.expo.io/versions/latest/sdk/firebase-analytics/
-          // Change this line to use another Mobile analytics SDK
+          // returns the options for the currently focused screen
+          // const options = navigationRef.current.getCurrentOptions();
 
-          // Analytics.setCurrentScreen(currentRouteName, currentRouteName);
-          /* await analytics().logScreenView({
+          if (previousRouteName !== currentRouteName) {
+            // The line below uses the expo-firebase-analytics tracker
+            // https://docs.expo.io/versions/latest/sdk/firebase-analytics/
+            // Change this line to use another Mobile analytics SDK
+
+            // Analytics.setCurrentScreen(currentRouteName, currentRouteName);
+            /* await analytics().logScreenView({
             screen_name: currentRouteName,
             screen_class: currentRouteName
           }); */
 
-          alert(`The route is changed to ${currentRouteName}`);
-        }
+            alert(`The route is changed to ${currentRouteName}`);
+          }
 
-        // Save the current route name for later comparison
-        routeNameRef.current = currentRouteName;
-      }}>
-      <Stack.Navigator headerMode="none">
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="AnalyticsScreen" component={AnalyticsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+          // Save the current route name for later comparison
+          routeNameRef.current = currentRouteName;
+        }}>
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="AnalyticsScreen" component={AnalyticsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      <Button
+        onPress={() =>
+          navigationRef.current &&
+          navigationRef.current.navigate('AnalyticsScreen')
+        }
+        title="Go to Analytics 2"
+      />
+    </View>
   );
 }
 
@@ -75,6 +93,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container2: {
+    flex: 1,
   },
   paragraph: {
     padding: 25,
@@ -86,3 +107,4 @@ const styles = StyleSheet.create({
 // https://rnfirebase.io/
 // https://firebase.google.com/docs/analytics/
 // https://docs.expo.io/versions/latest/sdk/firebase-analytics/
+// https://reactnavigation.org/docs/navigation-container/
