896a45b15e73334aa485f823347876d103fa9a7c
Omar M <ofattouh2@gmail.com>
2021-07-11 20:22:41 -0400

    Added HyperLink to Home screen

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import {StyleSheet, StatusBar, Button, Text, Image} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {Link} from '@react-navigation/native';
 
 function Home({navigation}) {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor="#fff" />
       <Text style={styles.paragraph}>Home Screen</Text>
+      <Text>{'\n'}</Text>
+
+      <Link to="/Profile">Profile Link</Link>
       <Text>{'\n'}</Text>
 
       <Button
